00657fc722f03b623fa5cd8b3f877532e9d7349d
dcassin2082 <dcassin2082@gmail.com>
2018-01-24 17:02:34 -0700

    Add files via upload

---
+++ BundleConfig.cs
@@ -0,0 +1,44 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace CustomerCrud
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                      "~/Scripts/angular.js",
+                      "~/Scripts/angular-resource.js",
+                      "~/Scripts/ui-bootstrap.js",
+                      "~/Scripts/ui-bootstrap-tpls.js",
+                      "~/Scripts/respond.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                      "~/Scripts/app/app.js",
+                      "~/Scripts/app/customer-controller.js",
+                       "~/Scripts/app/customer-factory.js",
+                        "~/Scripts/app/customer-service.js",
+                      "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/styles.css",
+                      "~/Content/ui-bootstrap-csp.css",
+                      "~/Content/site.css"));
+        }
+    }
+}
