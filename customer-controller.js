c2f2fcc50957e16becba375d68c0f83f75de1477
dcassin2082 <dcassin2082@gmail.com>
2018-01-25 15:57:41 -0700

    Update customer-controller.js

--- customer-controller.js
+++ customer-controller.js
@@ -45,6 +45,7 @@ app.controller('customerController', ['$scope', 'customerFactory', function ($sc
                 $scope.status = 'customer added sucessfully';
                 alert($scope.status);
                 $('#addModal').modal('hide');
+                $scope.refresh();
                 $scope.customer.Address = '';
                 $scope.customer.FirstName = '';
                 $scope.customer.LastName = '';
